53f4c6be00de6c37da6adf660a96de94aa8d0262
zach-davis <zach.davis@nyu.edu>
2020-09-17 18:25:16 -0700

    updating qualitative evaluation code

--- code/models/model_evaluation/cpj_dist_eval.R
+++ code/models/model_evaluation/cpj_dist_eval.R
@@ -1,23 +1,39 @@
 #------------------------------------
 # evaluating the distributions of conditional probability judgments
 #------------------------------------
+library(tidyverse)
 library(LaplacesDemon)   # gives kl-divergence through function KLD
 library(multimode)       # for evaluating the modality of a distribution
 library(mixtools)        # for identifying the parameters of mixed gaussians
 
+# KL-Divergence -----------------
 # basic KL-Divergence that can be used to evaluate how similar two networks are
 px <- dnorm(runif(100),0,1)
 py <- dnorm(runif(100),0.1,0.9)
 KLD(px,py)
 
-# generating some mixture data
-p1 <- rnorm(200, mean=0, sd=1)
-p2 <- rnorm(200, mean=4, sd=1)
-p3 <- rnorm(200, mean=8, sd=1)
-pz <- c(p1,p2,p3)
+# qualitative summary -------------------
+qualitative_eval <- function(judgments, bw=.1, threshold=.05) {
+    # number of modes
+    n_modes <- nmodes(judgments, bw=bw, full.result = FALSE)
+    # location of modes
+    loc_modes <- normalmixEM(pz, k = n_modes, fast=TRUE)
+    # had to get a little clever but threshold is distance from 0 or 1
+    # e.g. threshold of .05 finds all <.05 and >.95
+    extremes <- mean(abs(judgments - .5) > (.5 - threshold))
+    # variance
+    judge_var <- var(judgments)
+    
+    return(list(n_modes = n_modes,
+                loc_modes = loc_modes,
+                extremes = extremes,
+                judge_var = judge_var))
+}
 
-# testing modality
-nmodes(c(p1,p2,p3), bw=1, full.result=TRUE) %>% print
 
-# gaussian mixture modeling to identify modes
-normalmixEM(pz, fast=TRUE)
+
+
+
+
+
+
---
+++ code/models/model_evaluation/eval_script.R
@@ -0,0 +1,16 @@
+#------------------------
+# evaluating the distributions
+#------------------------
+# generating sample output from a model ------------------
+var.names = c ('x','y','z')
+ms = matrix (0, nrow=3, ncol=3, dimnames = list (var.names, var.names))
+ms['x','y'] = .5; ms['y','z'] = .75
+bs = c (.75, .5, .5)
+a <- param_variability(ms = ms, 
+                       bs = bs, 
+                       ms_conc = .1,
+                       bs_conc = .1,
+                       nSamples = 6)
+
+# I'm not sure how to do this systematically - we should discuss
+cpj_eval <- function(ms, bs, model) {}
