2e4f4ca3527b6ac3beabc9d0e6a459ad9eac7e5a
IvarKolvoort <54942252+IvarKolvoort@users.noreply.github.com>
2020-12-31 17:25:30 +0100

    added code to gen_cases.py to automatically generate all inferences

--- experiment/materials/network_generation/gen_cases.py
+++ experiment/materials/network_generation/gen_cases.py
@@ -1,5 +1,6 @@
 """
 Generating queries automatically
+"""
 
 import os
 from PIL import Image
@@ -8,35 +9,100 @@ import numpy as np
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
-"""
+
 
 def query_generator(vals):
-	base = Image.open('base.png')
+    base = Image.open('base.png')
 
-	# cause
-	X = Image.open('X/X_' + vals[0] + '.png')
-	X.thumbnail((275,275))
+    # cause
+    X = Image.open('X/X_' + vals[0] + '.png')
+    X.thumbnail((275,275))
 
-	# effects
-	img_size = (345,345)
-	Y = Image.open('Y/Y_' + vals[1] + '.png')
-	Y.thumbnail(img_size)
-	Z = Image.open('Z/Z_' + vals[2] + '.png')
-	Z.thumbnail(img_size)
+    # effects
+    img_size = (345,345)
+    Y = Image.open('Y/Y_' + vals[1] + '.png')
+    Y.thumbnail(img_size)
+    Z = Image.open('Z/Z_' + vals[2] + '.png')
+    Z.thumbnail(img_size)
 
-	base.paste(X, (0,30), X)
-	base.paste(Y, (372,0), Y)
-	base.paste(Z, (372,62), Z)
-	base.show()
+    base.paste(X, (0,30), X)
+    base.paste(Y, (372,0), Y)
+    base.paste(Z, (372,62), Z)
+    base.show()
 
+ #args are X=Y, Y=X1, Z=X2 in notation used for setup experiment slides
+ #econ values: Low, High, Small
 query_generator(['query', 'normal', 'unknown'])
 
 
+# to save the image instead of opening them.
+def query_generator2(vals, infnr, domain):
+    base = Image.open('base.png')
+
+    # cause
+    X = Image.open('X/X_' + vals[0] + '.png')
+    X.thumbnail((275,275))
+
+    # effects
+    img_size = (345,345)
+    Y = Image.open('Y/Y_' + vals[1] + '.png')
+    Y.thumbnail(img_size)
+    Z = Image.open('Z/Z_' + vals[2] + '.png')
+    Z.thumbnail(img_size)
+
+    base.paste(X, (0,30), X)
+    base.paste(Y, (372,0), Y)
+    base.paste(Z, (372,62), Z)
+    imname = domain+str(infnr)+vals[0]+vals[1]+vals[2]+'.png'
+    base.save(imname)
+    
 
 
+allinfs = np.array([['nonnormal', 'query', 'nonnormal'],
+           ['nonnormal', 'query', 'unknown'],
+           ['nonnormal', 'query', 'normal'],
+           ['nonnormal', 'query', 'nonnormal'],
+           ['nonnormal', 'query', 'unknown'],
+           ['nonnormal', 'query', 'normal'],
+           ['nonnormal', 'nonnormal', 'query'],
+           ['nonnormal', 'unknown', 'query'],
+           ['nonnormal', 'normal', 'query'],
+           ['nonnormal', 'nonnormal', 'query'],
+           ['nonnormal', 'unknown', 'query'],
+           ['nonnormal', 'normal', 'query'],
+           ['query', 'nonnormal', 'nonnormal'],
+           ['query', 'nonnormal', 'unknown'],
+           ['query', 'nonnormal', 'normal'],
+           ['query', 'nonnormal', 'nonnormal'],
+           ['query', 'nonnormal', 'unknown'],
+           ['query', 'nonnormal', 'normal'],
+           ['query', 'nonnormal', 'nonnormal'],
+           ['query', 'unknown', 'nonnormal'],
+           ['query', 'normal', 'nonnormal'],
+           ['query', 'nonnormal', 'nonnormal'],
+           ['query', 'unknown', 'nonnormal'],
+           ['query', 'normal', 'nonnormal']
+          ])
 
 
+def allinfsvals(allinfs, domainvals):
+    #changes nonnormal vals in allinfs to specified vals
+    #allinfs list of lists defined above
+    #domainvals the nonnormal values as [Y, X1, X2]
+    A = allinfs
+    for i in range(24):
+        for j in range(3):
+            if A[i,j]=='nonnormal':
+                A[i,j] = domainvals[j]
+    return A        
+    
+def all_query_generator(allinfs, domainvals, domain):
+    #make all images based on matrix of inf vals
+    A = allinfsvals(allinfs, domainvals)
+    for i in range(24):
+        query_generator2(A[i], i, domain)
 
+domain = 'econ'
+econvals1 = ['low', 'high', 'low'] #last one should be 'small'
 
-
-
+all_query_generator(allinfs, econvals1, domain)
