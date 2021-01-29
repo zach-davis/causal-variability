691555c3316c4a760d5acbb21502bde86fd22447
IvarKolvoort <54942252+IvarKolvoort@users.noreply.github.com>
2021-01-29 20:56:02 +0100

    updated analysis and plot script exp1

--- experiment/exp1/plots_analysis1.R
+++ experiment/exp1/plots_analysis1.R
@@ -80,10 +80,12 @@ datgroup <-  group_by(datlong, participant, inftype, diagnostic, information) %>
 
 res1 <- anovaBF(sdrespflip ~ diagnostic*information + participant, whichRandom="participant", data=datgroup, whichModels = 'withmain')
 res1 #so there is evidence for main effects and interaction
+res1[4]/res1[3] #BF for interaction
 
 res1.f <- lmer(sdrespflip ~ diagnostic*information + (1|participant), data=datgroup) #frequentist
 anova(res1.f)
-sjstats::anova_stats(res1.f) #dont know if correct for mixed model like this
+
+
 
 ############ plot posteriors for effects of inftype. maybe use this plot?
 samples1 <- posterior(res1, 1, iterations=10000, columnFilter="^$")
@@ -104,11 +106,11 @@ ggplot(samples1inftype, aes(x=value, y=name))+
 p2 <- ggplot(datgroup, aes(x=information))+
   geom_bar(stat = "summary", fun.y = "mean", aes(y=sdrespflip))+
   stat_summary(aes(y=sdrespflip), fun.data=mean_se, fun.args = list(mult=1), 
-               geom="pointrange",color="black", fatten=.2, size=2)+
+               geom="errorbar",color="black", width=.2, size=1)+
   geom_point(aes(y=meanrespflip/2), fun.y = "mean", stat = "summary",
              shape='-', size=30, stroke=20, color="grey40")+
   stat_summary(aes(y=meanrespflip/2), fun.data=mean_se, fun.args = list(mult=1), 
-               geom="pointrange", color="black", fatten=.2, size=2)+
+               geom="errorbar", color="black", width=.2, size=1)+
   scale_y_continuous(
     name = "Mean Standard Deviation",
     sec.axis = sec_axis(~.*2, name="Mean Response (in %)"))+
@@ -123,27 +125,46 @@ ggsave("figures/figure2.png", p2,  width = 30, height = 20, units = "cm")
 
 
 ############ test and plot accuracy (mean distance from correct response) and sd resp per inftype per pp
-cors <- by(datgroup, interaction(datgroup$diagnostic,datgroup$information), FUN = function(X) cor.test(X$sdrespflip, X$meanacc, method = "pearson"))
+cors <- by(datgroup, interaction(datgroup$diagnostic,datgroup$information), 
+           FUN = function(X) cor.test(X$sdrespflip, X$meanacc, method = "pearson"))
+
+# Default bayesian hypothesis test for correlation, employing JZS prior
+# from: Wetzels & Wagenmakers (2012) A default Bayesian hypothesis test for correlations and partial correlations
+jzs_corbf <- function(r ,n){
+  int=function(r,n,g){
+    (1+g)^((n-2)/2)*(1+(1-r^2)*g)^(-(n-1)/2)*
+      g^(-3/2)*exp(-n/(2*g))};
+  bf10=sqrt((n/2))/gamma(1/2)*integrate(int,lower=0,upper=Inf,r=r,n=n)$value;
+  return(bf10)
+  
+}
+corBFs<-data.frame(inftype=names(unlist(lapply(cors, "[[", c("estimate")))),
+                   cor=as.vector(unlist(lapply(cors, "[[", c("estimate")))),
+                   n=rep(29,6))
+corBFs$BF10 <- as.character(format(mapply(jzs_corbf, corBFs$cor, corBFs$n),digits=3, scientific=T))
+corBFs$information <- c("Consistent", "Consistent", "Incomplete", "Incomplete", "Inconsistent", "Inconsistent")
+corBFs$diagnostic <- c("Non-diagnostic", "Diagnostic", "Non-diagnostic", "Diagnostic", "Non-diagnostic", "Diagnostic")
+corBFs$BF10 <- paste("BF","=", corBFs$BF10)
 
 p3 <- ggplot(datgroup, aes(x=sdrespflip, y=meanacc))+
   geom_point()+
   geom_smooth(method='lm', se=F, colour="grey40")+
-  stat_cor(method = "pearson", label.x = 20, label.y = 45,
-           label.sep="\n", size=5)+
+  stat_cor(method = "pearson", label.x = 20, label.y = 42,
+           aes(label = ..r.label..), size=5)+
   #facet_wrap(facets=vars(diagnostic, information), nrow=2)+
   facet_grid(rows=vars(information), cols=vars(diagnostic), switch="both")+
+  geom_text(x=26.5, y=37, aes(label=BF10, fontface=2), size=4, data=corBFs)+
   theme_minimal(base_size = 23)+
-  theme(panel.spacing = unit(1, "lines"), 
+  theme(panel.spacing = unit(-.5, "lines"), 
         strip.background = element_blank(),
         strip.placement = "outside")+
   ylab("Mean accuracy")+
   xlab("Standard deviation")
 p3
-ggsave("figures/figure3.png", p3,  width = 20, height = 30, units = "cm")
+ggsave("figures/figure3.png", p3,  width = 20, height = 20, units = "cm")
+
 
 ### plot not separated by inftype
-
-
 
 ggplot(datgroup, aes(x=sdrespflip, y=meanacc))+
   geom_point()+
@@ -157,6 +178,39 @@ ggplot(datgroup, aes(x=sdrespflip, y=meanacc))+
   ylab("Mean accuracy")+
   xlab("Standard deviation")
 
+datoverallcorr <-  group_by(datlong, participant) %>% 
+  summarise(
+    meanacc = mean(acc, na.rm = TRUE),
+    sdrespflip = sd(respflip, na.rm = TRUE)
+  )
+
+overallcor <- cor.test(datoverallcorr$sdrespflip, datoverallcorr$meanacc, method = "pearson")
+BFoverallcor <- jzs_corbf(overallcor$estimate, 29)
+BFoverallcor
+
+### plot separated by information and diagnostic factors
+
+p31 <- ggplot(datgroup, aes(x=sdrespflip, y=meanacc, group=diagnostic, colour=diagnostic))+
+  geom_point()+
+  geom_smooth(method='lm', se=F)+
+  stat_cor(method = "pearson", label.x = 20,
+           label.sep=",", size=5)+
+  theme_minimal(base_size = 23)+
+  theme(panel.spacing = unit(1, "lines"))+
+  ylab("Mean accuracy")+
+  xlab("Standard deviation")
+
+p32 <- ggplot(datgroup, aes(x=sdrespflip, y=meanacc, group=information, colour=information))+
+  geom_point()+
+  geom_smooth(method='lm', se=F)+
+  stat_cor(method = "pearson", label.x = 20,
+           label.sep=",", size=5)+
+  theme_minimal(base_size = 23)+
+  theme(panel.spacing = unit(1, "lines"))+
+  ylab("Mean accuracy")+
+  xlab("Standard deviation")
+
+ggarrange(p31, p32, nrow=2)
 
 
 ### plot markov violations
@@ -170,14 +224,51 @@ datpp <-  group_by(datlong[datlong$diagnostic=="Non-diagnostic",], participant) 
 datpp$sdgroup <- Hmisc::cut2(datpp$sdrespflipmrkv, g=3)
 datpp$sdgroup <- factor(datpp$sdgroup,
                         levels=levels(datpp$sdgroup),
-                        labels=c("low variability", "medium variability", "high variability"))
+                        labels=c("Low variability", "Medium variability", "High variability"))
 datgroup <- merge(datgroup, datpp, by="participant")
 
-ggplot(datgroup[datgroup$diagnostic=="Non-diagnostic",], 
-       aes(y=meanrespflip, x=inftype, group=participant))+
-  facet_wrap(facets=vars(sdgroup))+
-  geom_line()
+datgroupmean<-  group_by(datgroup, sdgroup, inftype, diagnostic, information) %>% 
+  summarise(
+    meangroupmean = mean(meanrespflip, na.rm = TRUE),
+  )  
+datgroup <- merge(datgroup, datgroupmean)
 
+p4 <- ggplot(datgroup[datgroup$diagnostic=="Non-diagnostic",])+
+  facet_wrap(facets=vars(sdgroup))+
+  geom_line(aes(y=meanrespflip, x=information, group=participant),
+            color='grey40')+
+  #geom_point(aes(y=meanrespflip, x=information, group=participant))+
+  geom_line(aes(y=meangroupmean, x=information, group=participant), 
+           size =3)+
+  stat_summary(aes(y=meanrespflip, x=information), fun.data=mean_se, fun.args = list(mult=1), 
+               geom="errorbar", color="black", width=.2, size=1)+
+  geom_segment(aes(y=80, yend=80, x=1, xend=3),
+               color='grey40', linetype="dashed", size=2)+
+  ggtitle("Markov violations (non-diagnostic inferences)")+
+  ylab("Mean response (in %)")+
+  theme_minimal(base_size = 24)+
+  theme(panel.spacing = unit(0, "lines"), 
+        strip.background = element_blank(),
+        strip.placement = "outside",
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 40, hjust=1))
+
+
+p4
+ggsave("figures/figure4.png", p4,  width = 30, height = 20, units = "cm")
+
+## statistics influence variability on markov violations
+## (this could be done better using mixed model, long data, and meansd as continuous covariate)
+
+datlong <- merge(datlong, datpp, by="participant")## add sdgroup var to long data
+res3 <- anovaBF(respflip ~ information*sdgroup + participant, whichRandom="participant", 
+                data=datlong[datlong$diagnostic=="Non-diagnostic",], whichModels = 'withmain')
+res3 #so there is evidence for main information and interaction
+res3[4]/res3[3] #BF for interaction.
+
+res1.f <- lmer(respflip ~ information*sdgroup + (1|participant), data=datlong[datlong$diagnostic=="Non-diagnostic",]) #frequentist
+anova(res1.f)
+summary(res1.f)
 
 
 
